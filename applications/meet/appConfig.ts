0f568253d236bed49f94aa0aa430e4d0836bdce2
István Barna <istvan.barna@proton.ch>
2025-11-05 10:40:10 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Mattias Svanström <mmso@mmso.se>

--- applications/meet/appConfig.ts
+++ applications/meet/appConfig.ts
@@ -1,6 +1,6 @@
 const appConfig = {
     clientType: 11,
-    sentry: 'https://8f130b1135494dc8a92a231103dc34e3@sentry-new.protontech.ch/85',
+    sentry: 'https://8f130b1135494dc8a92a231103dc34e3@sentry/85',
     sentryDesktop: 'https://8f130b1135494dc8a92a231103dc34e3@sentry-new.protontech.ch/85',
 };
 
